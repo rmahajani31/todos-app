47a38e8747f3aa6b3fdaf3682b12d14fe7c4df17
Rishabh Mahajani <rmahajani3@gatech.edu>
2017-08-16 18:03:55 -0400

    Refactored code by putting all the express routes in seperate file

--- routes/api.js
+++ routes/api.js
@@ -1,9 +1,110 @@
 const express = require('express');
+const {mongoose} = require('./db/mongoose');
+const {Todo} = require('./models/todos');
+const bodyParser = require('body-parser');
+const {ObjectID} = require('mongodb');
+const _ = require('lodash');
 
 const router = express.Router();
 
+router.use(bodyParser.json());
+
 router.get('/', (request, response, next) => {
     response.send('api');
+});
+
+// POST new todo to the mongo db
+router.post('/todos', (request, response) => {
+    const newTodo = new Todo({
+        text: request.body.text
+    });
+    newTodo.save()
+    .then((todo) => {
+        response.send(todo);
+    })
+    .catch((error) => {
+        response.status(400).send(error);
+    });
+});
+
+// GET all todos in the mongo db
+router.get('/todos', (request, response) => {
+    Todo.find()
+    .then((todos) => {
+        response.send({todos});
+    })
+    .catch((error) => {
+        response.status(400).send(error);
+    });
+});
+
+// GET todo by id in mongo db
+router.get('/todos/:id', (request, response) => {
+    const id = request.params.id;
+
+    if(!ObjectID.isValid(id)) {
+        response.status(404).send({'Error': 'Invalid ID'});
+    }
+
+    Todo.findById(id)
+    .then((todo) => {
+        if (!todo) {
+            response.status(404).send({'Error': 'No todo for corresponding id'});
+        }
+        response.send(todo);
+    })
+    .catch((error) => {
+        response.status(400).send(error);
+    });
+});
+
+// DELETE todo from the mongo server
+router.delete('/todos/:id', (request, response) => {
+    const id = request.params.id;
+
+    if (!ObjectID.isValid(id)) {
+        response.status(404).send({'Error': 'Invalid ID'});
+    }
+
+    Todo.findByIdAndRemove(id)
+    .then((todo) => {
+        if(!todo) {
+            response.status(404).send({'Error': 'No todo for corresponding id'});
+        }
+        response.send(todo);
+    })
+    .catch((error) => {
+        response.status(400).send(error);
+    });
+});
+
+// PATCH todo in mongo db
+router.patch('/todos/:id', (request, response) => {
+    const id = request.params.id;
+    const body = _.pick(request.body, ['text', 'completed']);
+
+    if (!ObjectID.isValid(id)) {
+        response.status(404).send({'Error': 'Invalid ID'});
+    }
+
+    if(_.isBoolean(body.completed) && body.completed) {
+        body.completedAt = new Date().getTime();
+    }
+
+    Todo.findByIdAndUpdate(id, {
+        $set: body
+    }, {
+        new: true
+    })
+    .then((todo) => {
+        if(!todo) {
+            response.status(404).send({'Error': 'No todo for corresponding id'});
+        }
+        response.send(todo);
+    })
+    .catch((error) => {
+        response.status(400).send(error);
+    }); 
 });
 
 module.exports = {
--- server/server.js
+++ server/server.js
@@ -1,9 +1,4 @@
 const express = require('express');
-const {mongoose} = require('./db/mongoose');
-const {Todo} = require('./models/todos');
-const bodyParser = require('body-parser');
-const {ObjectID} = require('mongodb');
-const _ = require('lodash');
 const ejs = require('ejs');
 const path = require('path');
 const mainRouter = require('../routes/index');
@@ -23,110 +18,11 @@ app.engine('html', ejs.renderFile);
 // Set static folder
 app.use(express.static(path.join(__dirname, '../client')));
 
-// Set up body parser as middleware
-app.use(bodyParser.json());
-
 // Set up routes
 app.use('/', mainRouter.router);
 app.use('/api', apiRouter.router);
 
 const port = process.env.PORT || 3000;
-
-// POST new todo to the mongo db
-app.post('/todos', (request, response) => {
-    const newTodo = new Todo({
-        text: request.body.text
-    });
-    newTodo.save()
-    .then((todo) => {
-        response.send(todo);
-    })
-    .catch((error) => {
-        response.status(400).send(error);
-    });
-});
-
-// GET all todos in the mongo db
-app.get('/todos', (request, response) => {
-    Todo.find()
-    .then((todos) => {
-        response.send({todos});
-    })
-    .catch((error) => {
-        response.status(400).send(error);
-    });
-});
-
-// GET todo by id in mongo db
-app.get('/todos/:id', (request, response) => {
-    const id = request.params.id;
-
-    if(!ObjectID.isValid(id)) {
-        response.status(404).send({'Error': 'Invalid ID'});
-    }
-
-    Todo.findById(id)
-    .then((todo) => {
-        if (!todo) {
-            response.status(404).send({'Error': 'No todo for corresponding id'});
-        }
-        response.send(todo);
-    })
-    .catch((error) => {
-        response.status(400).send(error);
-    });
-});
-
-// DELETE todo from the mongo server
-app.delete('/todos/:id', (request, response) => {
-    const id = request.params.id;
-
-    if (!ObjectID.isValid(id)) {
-        response.status(404).send({'Error': 'Invalid ID'});
-    }
-
-    Todo.findByIdAndRemove(id)
-    .then((todo) => {
-        if(!todo) {
-            response.status(404).send({'Error': 'No todo for corresponding id'});
-        }
-        response.send(todo);
-    })
-    .catch((error) => {
-        response.status(400).send(error);
-    });
-});
-
-// PATCH todo in mongo db
-app.patch('/todos/:id', (request, response) => {
-    const id = request.params.id;
-    const body = _.pick(request.body, ['text', 'completed']);
-
-    if (!ObjectID.isValid(id)) {
-        response.status(404).send({'Error': 'Invalid ID'});
-    }
-
-    if(_.isBoolean(body.completed) && body.completed) {
-        body.completedAt = new Date().getTime();
-    }
-
-    Todo.findByIdAndUpdate(id, {
-        $set: body
-    }, {
-        new: true
-    })
-    .then((todo) => {
-        if(!todo) {
-            response.status(404).send({'Error': 'No todo for corresponding id'});
-        }
-        response.send(todo);
-    })
-    .catch((error) => {
-        response.status(400).send(error);
-    }); 
-});
-
-
 
 app.listen(port, () => {
     console.log(`Express server listening on port ${port}`);
