2344a382c853b1ef393867a8a419cbddd06bae98
Rishabh Mahajani <rmahajani3@gatech.edu>
2017-08-16 22:58:01 -0400

    Added edit functionality with the express PATCH api

--- client/src/app.module.ts
+++ client/src/app.module.ts
@@ -5,10 +5,11 @@ import { HttpModule } from '@angular/http';
 import { FormsModule } from '@angular/forms';
 import { TodosComponent } from './todos.component';
 import { RegisterComponent } from './register.component';
+import { EditComponent } from './edit.component';
 
 @NgModule({
     imports: [BrowserModule, HttpModule, FormsModule],
-    declarations: [AppComponent, TodosComponent, RegisterComponent],
+    declarations: [AppComponent, TodosComponent, RegisterComponent, EditComponent],
     bootstrap: [AppComponent]
 })
 export class AppModule {}
