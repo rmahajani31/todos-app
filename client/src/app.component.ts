a73c0637991d191b74876cfd4fd91dd87a7c911b
Rishabh Mahajani <rmahajani3@gatech.edu>
2017-08-16 22:28:55 -0400

    Applying styles to the app.html page

--- client/src/app.component.ts
+++ client/src/app.component.ts
@@ -3,6 +3,7 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'my-app',
-    templateUrl: '../views/app.html'
+    templateUrl: '../views/app.html',
+    styleUrls: ['../styles/app.css']
 })
 export class AppComponent { }
